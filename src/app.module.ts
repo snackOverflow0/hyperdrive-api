0d791056da3e2dbd50f91ce8bd7bfb926cc7d246
snackOverflow0 <castelochristopher00@gmail.com>
2026-06-13 15:54:10 +0800

    chore: finalize logistics core infrastructure with public static asset upload servicing mapping

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,21 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { TripsModule } from './trips/trips.module';
 import { StorageModule } from './storage/storage.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
-
   ConfigModule.forRoot({ isGlobal: true }),
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'uploads'),
+    serveRoot: '/uploads'
+  }),
+
   PrismaModule,
   AuthModule,
   TripsModule,
   StorageModule
-
   ],
   
   controllers: [AppController],
